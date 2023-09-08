a55557f1a39a127da0af420607434e497eaa1c94
“Samudhi1220” <samudhirandula@icloud.com>
2023-09-08 21:17:54 +0530

    Initial Project

---
+++ src/main/java/AppInitializer.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class AppInitializer extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/loginForm.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
---
+++ src/main/java/lk/ijse/fisheryharbour/controller/LoginFormController.java
@@ -0,0 +1,8 @@
+package lk.ijse.fisheryharbour.controller;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class LoginFormController {
+}
