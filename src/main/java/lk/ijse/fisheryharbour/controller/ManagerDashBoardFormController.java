78b626907a2dd8924d18cddd6b8dfad8977b03a7
“Sithum1220” <sithum.imesh20020506@icloud.com>
2023-09-11 02:26:09 +0530

    Foget Password Form Ui

--- src/main/java/lk/ijse/fisheryharbour/controller/ManagerDashBoardFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/ManagerDashBoardFormController.java
@@ -1,4 +1,30 @@
 package lk.ijse.fisheryharbour.controller;
 
-public class ManagerDashBoardFormController {
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManagerDashBoardFormController implements Initializable {
+    public JFXButton btnDashboard;
+    public JFXButton btnEmployee;
+    public Label btnDashboardText;
+    public ImageView btnImage;
+
+    public void dashboardOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void employeeOnAction(ActionEvent actionEvent) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
