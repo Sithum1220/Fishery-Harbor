e11f9f001ccaba1a1b0e366ee6cbc934d244a570
Sithum Imesh <sithum.imesh20020506@icloud.com>
2023-09-11 10:30:24 +0530

    Dashboard Btn update

--- src/main/java/lk/ijse/fisheryharbour/controller/ManagerDashBoardFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/ManagerDashBoardFormController.java
@@ -4,27 +4,76 @@ import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ManagerDashBoardFormController implements Initializable {
+
+
     public JFXButton btnDashboard;
-    public JFXButton btnEmployee;
+    public ImageView btnDashboardImg;
     public Label btnDashboardText;
-    public ImageView btnImage;
+    public JFXButton btnEmployee;
+    public ImageView btnEmployeeImg;
+    public Label btnEmployeeText;
+    public JFXButton btnSupplier;
+    public ImageView btnSupplierImg;
+    public Label btnSupplierText;
+    public JFXButton btnOwner;
+    public Label btnOwnerText;
+    public JFXButton btnStock;
+    public ImageView btnStockImg;
+    public Label btnStockText;
+    public JFXButton btnReport;
+
 
     public void dashboardOnAction(ActionEvent actionEvent) {
-
+        btnSelected(btnDashboard,btnDashboardText,btnDashboardImg,"category.png");
+        btnUnselected(btnEmployee,btnEmployeeText,btnEmployeeImg,"profile-2user.png");
     }
 
-    public void employeeOnAction(ActionEvent actionEvent) {
-
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        btnSelected(btnDashboard,btnDashboardText,btnDashboardImg,"category.png");
+    }
+
+
+    void btnSelected(JFXButton button,Label label,ImageView imageView,String path){
+        button.setStyle(
+                "-fx-background-color: #EDF3FF;" +
+                "-fx-background-radius: 12px;");
+        label.setStyle("-fx-text-fill: #1351CB;" +
+                "-fx-font-size: 16px");
+        imageView.setImage(new Image("img/icon/"+path));
+    }
+    void btnUnselected(JFXButton button,Label label,ImageView imageView,String path){
+        button.setStyle(
+                "-fx-background-radius: 12px;");
+        label.setStyle("-fx-font-weight: 500;" +
+                "-fx-font-size: 16px;" +
+                "-fx-text-fill: #A3A3A3;");
+        imageView.setImage(new Image("img/icon/"+path));
+    }
+
+    public void btnEmployeeOnAction(ActionEvent actionEvent) {
+        btnSelected(btnEmployee,btnEmployeeText,btnEmployeeImg,"profile-2user-2.png");
+        btnUnselected(btnDashboard,btnDashboardText,btnDashboardImg,"category-2.png");
+    }
+
+    public void btnSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOwnerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStockOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReportOnAction(ActionEvent actionEvent) {
 
     }
 }
