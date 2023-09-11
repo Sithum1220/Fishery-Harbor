76bd916e76ff6a78e73cc169eec169b93b1e5205
Janindu Lokitha <ljanindulokitha@gmail.com>
2023-09-11 18:22:07 +0530

    update

--- src/main/java/lk/ijse/fisheryharbour/controller/AdminAddFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/AdminAddFormController.java
@@ -1,12 +1,27 @@
 package lk.ijse.fisheryharbour.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import lk.ijse.fisheryharbour.utill.Navigation;
 
 import java.io.IOException;
 
 public class AdminAddFormController {
+    public JFXTextField txtFirstName;
+    public JFXTextField txtLastName;
+    public JFXTextField txtMobile;
+    public JFXTextField txtNic;
+    public JFXComboBox cmbRole;
+    public JFXTextField txtHouseNo;
+    public JFXTextField txtStreet;
+    public JFXTextField txtCity;
+    public JFXTextField txtUserName;
+    public JFXTextField txtPassword;
+    public AnchorPane txtEmail;
+
     public void closeOnMouseClick(MouseEvent event) {
     }
 
