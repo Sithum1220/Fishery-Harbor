9133a55fbc7df369e8405ee5ae73321cb41a3cda
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-09-12 22:33:04 +0530

    userVerifyFormController update

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/UserVerifyFormController.java
@@ -0,0 +1,27 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class UserVerifyFormController {
+    public JFXButton btnBack;
+    public JFXTextField txtOtp1;
+    public JFXTextField txtOtp2;
+    public JFXTextField txtOtp3;
+    public JFXTextField txtOtp4;
+    public JFXButton btnVerify;
+
+    public void backBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void closeOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void verifyBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void minimizeOnMouseClick(MouseEvent mouseEvent) {
+    }
+}
