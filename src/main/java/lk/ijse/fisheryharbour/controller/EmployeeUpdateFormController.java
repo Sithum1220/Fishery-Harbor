4c772c6f50afecf76158842663ac6ffb5b2859b0
Janindu Lokitha <ljanindulokitha@gmail.com>
2023-09-12 23:50:23 +0530

    update

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/EmployeeUpdateFormController.java
@@ -0,0 +1,27 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class EmployeeUpdateFormController {
+    public JFXTextField txtFirstName;
+    public JFXTextField txtLastName;
+    public JFXTextField txtMobile;
+    public JFXTextField txtEmail;
+    public JFXTextField txtNic;
+    public JFXComboBox cmbRole;
+    public JFXTextField txtHouseNo;
+    public JFXTextField txtStreet;
+    public JFXTextField txtCity;
+
+    public void btnAsAAdminOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnEmployeeAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void closeOnMouseClick(MouseEvent mouseEvent) {
+    }
+}
