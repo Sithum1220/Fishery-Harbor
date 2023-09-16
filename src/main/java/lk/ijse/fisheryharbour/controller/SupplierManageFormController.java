5a5860756e8d5882a7ae20c7150bbad651d7203d
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-09-16 22:31:20 +0530

    SupplierManageForm UI

--- src/main/java/lk/ijse/fisheryharbour/controller/SupplierManageFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/SupplierManageFormController.java
@@ -1,11 +1,8 @@
 package lk.ijse.fisheryharbour.controller;
 
-import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 
 public class SupplierManageFormController {
-    public JFXButton btnAddSupplier;
-
     public void btnSupplierAddOnAction(ActionEvent actionEvent) {
     }
 }
