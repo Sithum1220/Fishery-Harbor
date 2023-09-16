3a202efaec92e475cd4141e18ff6f5c6c99c11b0
Sithum Imesh <sithum.imesh20020506@icloud.com>
2023-09-16 22:47:57 +0530

    Update

--- src/main/java/lk/ijse/fisheryharbour/controller/SupplierManageFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/SupplierManageFormController.java
@@ -1,8 +1,12 @@
 package lk.ijse.fisheryharbour.controller;
 
 import javafx.event.ActionEvent;
+import lk.ijse.fisheryharbour.utill.Navigation;
+
+import java.io.IOException;
 
 public class SupplierManageFormController {
-    public void btnSupplierAddOnAction(ActionEvent actionEvent) {
+    public void btnSupplierAddOnAction(ActionEvent actionEvent) throws IOException {
+        Navigation.popupPane("SupplierAddForm.fxml");
     }
 }
