94f65961b1e605fb33e8f940e49a1a9122917687
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-09-17 22:24:18 +0530

    OwnerDetailView UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/OwnerDetailsViewFormController.java
@@ -0,0 +1,37 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class OwnerDetailsViewFormController {
+    public ImageView closeImg;
+    public JFXTextField txtName;
+    public JFXTextField txtMobile;
+    public JFXTextField txtEmail;
+    public JFXTextField txtNic;
+    public JFXTextField txtAddress;
+    public JFXTextField txtRole;
+    public Text txtBlueCancel;
+    public Text txtCancel;
+
+    public void closeOnMouseExit(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnMouseExit(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+}
