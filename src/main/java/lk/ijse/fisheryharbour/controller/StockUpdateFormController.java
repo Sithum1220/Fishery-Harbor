4a567f43954a0d72d18d77e110f7aae29b5f8259
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-09-18 11:45:19 +0530

    StockUpdateForm UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/StockUpdateFormController.java
@@ -0,0 +1,37 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class StockUpdateFormController {
+    public ImageView closeImg;
+    public Text txtCancel;
+    public Text txtBlueCancel;
+    public JFXTextField txtProductName;
+    public JFXTextField txtQuantityOnHand;
+    public JFXTextField txtUnitPrice;
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+
+    public void closeOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseExit(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnMouseExit(MouseEvent mouseEvent) {
+    }
+
+    public void btnSupplierUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+}
