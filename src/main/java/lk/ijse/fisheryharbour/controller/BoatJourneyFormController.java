c1a4524ae9825a90a61aeb6d163f5ddc42ca16db
chalaniChamodya <139869863+chalaniChamodya@users.noreply.github.com>
2023-09-18 13:26:54 +0530

    BoatJourneyForm UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/BoatJourneyFormController.java
@@ -0,0 +1,14 @@
+package lk.ijse.fisheryharbour.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.VBox;
+
+public class BoatJourneyFormController {
+    public VBox vBox;
+
+    public void btnBoatJourneyAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBoatEmployeeDetailsOnAction(ActionEvent actionEvent) {
+    }
+}
