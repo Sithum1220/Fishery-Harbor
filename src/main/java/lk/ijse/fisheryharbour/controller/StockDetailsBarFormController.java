e44f2935ba032015ffa135b18d05689eb66cc1b3
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-09-18 11:07:49 +0530

    StockDetailsBarForm UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/StockDetailsBarFormController.java
@@ -0,0 +1,33 @@
+package lk.ijse.fisheryharbour.controller;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class StockDetailsBarFormController {
+    public Text id;
+    public Text CompanyName;
+    public Text Location;
+    public Text email;
+    public ImageView updateImg;
+    public ImageView deleteImg;
+    public ImageView viewImg;
+
+    public void detailsOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void viewOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void viewOnMouseExited(MouseEvent mouseEvent) {
+    }
+
+    public void deleteOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void deleteOnMouseExited(MouseEvent mouseEvent) {
+    }
+
+    public void deleteOnMouseEntered(MouseEvent mouseEvent) {
+    }
+}
