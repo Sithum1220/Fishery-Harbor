8782c74f2681618f64a0631ea27c12769366fd3f
chalaniChamodya <139869863+chalaniChamodya@users.noreply.github.com>
2023-09-18 11:04:35 +0530

    StockAddForm UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/StockAddFormController.java
@@ -0,0 +1,38 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class
+StockAddFormController {
+    public ImageView closeImg;
+    public JFXTextField txtProductName;
+    public JFXTextField txtQuantityOnHand;
+    public JFXTextField txtUnitPrice;
+    public Text txtCancel;
+    public Text txtCancel1;
+
+    public void closeOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnMouseExit(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseExit(MouseEvent mouseEvent) {
+    }
+
+    public void btnSupplierAddOnAction(ActionEvent actionEvent) {
+    }
+}
