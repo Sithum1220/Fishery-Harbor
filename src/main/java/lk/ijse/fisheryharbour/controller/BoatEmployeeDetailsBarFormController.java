dcc6363291ba634557176de3440da0484756bd86
chalaniChamodya <139869863+chalaniChamodya@users.noreply.github.com>
2023-09-18 16:15:08 +0530

    BoatEmployeeDetailsBarForm UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/BoatEmployeeDetailsBarFormController.java
@@ -0,0 +1,43 @@
+package lk.ijse.fisheryharbour.controller;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class BoatEmployeeDetailsBarFormController {
+    public Text id;
+    public Text BoatId;
+    public Text Name;
+    public Text Mobile;
+    public Text Email;
+    public ImageView updateImg;
+    public ImageView deleteImg;
+    public ImageView viewImg;
+
+    public void updateOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void updateOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void updateOnMouseExited(MouseEvent mouseEvent) {
+    }
+
+    public void deleteOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void deleteOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void deleteOnMouseExited(MouseEvent mouseEvent) {
+    }
+
+    public void detailsOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void viewOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void viewOnMouseExited(MouseEvent mouseEvent) {
+    }
+}
