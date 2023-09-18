e25a888a78917dd5062f30abce8fb750884bc9f8
chalaniChamodya <139869863+chalaniChamodya@users.noreply.github.com>
2023-09-18 14:52:40 +0530

    BoatEmployeeDetailsForm UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/BoatEmployeeDetailsFormController.java
@@ -0,0 +1,14 @@
+package lk.ijse.fisheryharbour.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.VBox;
+
+public class BoatEmployeeDetailsFormController {
+    public VBox vBox;
+
+    public void btnBoatEmployeeAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBoatJourneyOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/lk/ijse/fisheryharbour/controller/BoatManageFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/BoatManageFormController.java
@@ -1,8 +1,11 @@
 package lk.ijse.fisheryharbour.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.layout.VBox;
 
 public class BoatManageFormController {
+    public VBox vBox;
+
     public void btnOwnerAddOnAction(ActionEvent actionEvent) {
     }
 
