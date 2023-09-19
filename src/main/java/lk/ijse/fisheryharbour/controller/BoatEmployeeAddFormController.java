d51da5939601841161b49fbe0b44986a121ab095
chalaniChamodya <139869863+chalaniChamodya@users.noreply.github.com>
2023-09-19 18:19:12 +0530

    BoatEmployeeAddForm UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/BoatEmployeeAddFormController.java
@@ -0,0 +1,42 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class BoatEmployeeAddFormController {
+    public ImageView closeImg;
+    public JFXTextField txtFirstName;
+    public JFXTextField txtLastName;
+    public JFXTextField txtBoatEmployeeID;
+    public JFXTextField txtNIC;
+    public JFXTextField txtMobile;
+    public JFXComboBox cmbRole;
+    public JFXTextField txtHouseNo;
+    public JFXTextField txtStreet;
+    public JFXTextField txtCity;
+    public JFXTextField txtBoatID;
+
+    public void closeOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseExit(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnMouseExit(MouseEvent mouseEvent) {
+    }
+
+    public void btnEmployeeAddOnAction(ActionEvent actionEvent) {
+    }
+}
