fb062cfc2c0cea6d52ebecc9ffe955735d1b837e
chalaniChamodya <139869863+chalaniChamodya@users.noreply.github.com>
2023-09-21 00:48:18 +0530

    OrderDetails UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/OrderDetailsFormController.java
@@ -0,0 +1,20 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.VBox;
+
+public class OrderDetailsFormController {
+    public VBox vBox;
+    public JFXButton btnAddSupplierOrder;
+    public JFXButton btnAddOwnerOrder;
+
+    public void btnEnterIdAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddSupplierOrderAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddOwnerOrderAddOnAction(ActionEvent actionEvent) {
+    }
+}
