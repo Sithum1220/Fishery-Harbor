ab2856f8c3443cd8abcdaa2dfdf590c043315569
chalaniChamodya <139869863+chalaniChamodya@users.noreply.github.com>
2023-09-20 23:12:02 +0530

    Rent UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/TaxDetailsUpdateFormController.java
@@ -0,0 +1,37 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class TaxDetailsUpdateFormController {
+    public ImageView closeImg;
+    public JFXTextField txtBoatID;
+    public JFXTextField txtTaxType;
+    public JFXTextField txtTaxFee;
+    public Text txtCancel;
+    public Text txtBlueCancel;
+
+    public void closeOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseExited(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnMouseExited(MouseEvent mouseEvent) {
+    }
+
+    public void btnEmployeeUpdateOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/lk/ijse/fisheryharbour/controller/TaxDetailsViewFormController.java
@@ -0,0 +1,34 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class TaxDetailsViewFormController {
+    public ImageView closeImg;
+    public JFXTextField txtBoatID;
+    public JFXTextField txtTaxType;
+    public JFXTextField txtTaxFee;
+    public Text txtCancel;
+    public Text txtBlueCancel;
+
+    public void closeOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseExit(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnMouseExit(MouseEvent mouseEvent) {
+    }
+}
