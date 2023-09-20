59709596a80ef2837c5654dd20117ff40b9924d6
chalaniChamodya <139869863+chalaniChamodya@users.noreply.github.com>
2023-09-20 19:30:21 +0530

    BoatJourneyUPdateForm UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/AttendanceDetailsViewFormController.java
@@ -0,0 +1,36 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class AttendanceDetailsViewFormController {
+    public ImageView closeImg;
+    public JFXTextField txtName;
+    public JFXTextField txtMobile;
+    public JFXTextField txtEmail;
+    public JFXTextField txtNic;
+    public Text txtCancel;
+    public Text txtBlueCancel;
+    public JFXTextField txtRole;
+
+    public void closeOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+
+    public void closeOnMouseExit(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnMouseEnterd(MouseEvent mouseEvent) {
+    }
+
+    public void btnCancelOnMouseExit(MouseEvent mouseEvent) {
+    }
+}
