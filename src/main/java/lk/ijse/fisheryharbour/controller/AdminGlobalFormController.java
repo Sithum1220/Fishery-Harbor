e2b73fe1f4f221f0615f36235f5961e1e6dda034
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-09-20 21:03:35 +0530

    Update Admin UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/AdminGlobalFormController.java
@@ -0,0 +1,56 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class AdminGlobalFormController {
+    public Pane pagingPane;
+    public JFXButton btnDashboard;
+    public ImageView btnDashboardImg;
+    public Label btnDashboardText;
+    public JFXButton btnAttendance;
+    public ImageView btnEmployeeImg;
+    public Label btnEmployeeText;
+    public JFXButton btnBoatManage;
+    public ImageView btnSupplierImg;
+    public Label btnSupplierText;
+    public JFXButton btnRent;
+    public ImageView btnOwnerImg;
+    public Label btnOwnerText;
+    public JFXButton btnStock;
+    public ImageView btnStockImg;
+    public Label btnStockText;
+    public JFXButton btnLogout;
+    public ImageView btnLogoutImg;
+    public Label btnLogoutText;
+    public Pane crudPane;
+    public ImageView popupPane;
+
+    public void dashboardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAttendenceOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBoatManageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRentAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStockOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogoutOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void btnLogoutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogoutOnMouseExit(MouseEvent mouseEvent) {
+    }
+}
