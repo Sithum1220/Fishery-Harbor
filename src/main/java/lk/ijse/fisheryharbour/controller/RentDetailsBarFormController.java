c69b97df5007e75021c5b0c07a98cc840a6e288c
chalaniChamodya <139869863+chalaniChamodya@users.noreply.github.com>
2023-09-20 21:02:35 +0530

    RentDetailsBarForm UI

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/RentDetailsBarFormController.java
@@ -0,0 +1,43 @@
+package lk.ijse.fisheryharbour.controller;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class RentDetailsBarFormController {
+    public Text txtID;
+    public Text txtOwnerID;
+    public Text txtRentType;
+    public Text txtFee;
+    public Text txtDate;
+    public ImageView updateImg;
+    public ImageView deleteImg;
+    public ImageView viewImg;
+
+    public void updateOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void updateOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void updateOnMouseExited(MouseEvent mouseEvent) {
+    }
+
+    public void deleteOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void deleteOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void deleteOnMouseExited(MouseEvent mouseEvent) {
+    }
+
+    public void detailsOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void viewOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void viewOnMouseExited(MouseEvent mouseEvent) {
+    }
+}
