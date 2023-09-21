170d24574fdb89bb150b98e4c3f6254eb96cf926
Sithum Imesh <sithum.imesh20020506@icloud.com>
2023-09-21 23:04:40 +0530

    issue fixed

--- src/main/java/lk/ijse/fisheryharbour/controller/TaxManageFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/TaxManageFormController.java
@@ -1,5 +1,8 @@
 package lk.ijse.fisheryharbour.controller;
 
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,8 +13,14 @@ import lk.ijse.fisheryharbour.model.QueryModel;
 import lk.ijse.fisheryharbour.model.TaxModel;
 import lk.ijse.fisheryharbour.utill.Navigation;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -64,6 +73,17 @@ public class TaxManageFormController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
             allTaxId();
+
+            Thread thread = new Thread(()->{
+                while (true){
+                    renameImgPhone();
+                    imgTranfer();
+                    qrDecode();
+                    deleteImgPhone();
+                    deleteImgLap();
+                }
+            });
+            thread.start();
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -77,34 +97,153 @@ public class TaxManageFormController implements Initializable {
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private void innovativePopup() throws SQLException, ClassNotFoundException {
+    private void innovativePopup() throws SQLException, ClassNotFoundException, IOException {
         QueryModel queryModel = new QueryModel();
         boolean isPay = queryModel.checkTaxPay(innovativeBoatId);
         innovativePopupPane.setVisible(true);
 
-        Thread thread = new Thread(() -> {
-            if (isPay) {
-                try {
-                    FXMLLoader loader = new FXMLLoader(EmployeeManageFormController.class.getResource("/view/PaidPopupForm.fxml"));
-                    Parent root = loader.load();
-                    innovativePopupPane.getChildren().add(root);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                try {
-                    FXMLLoader loader = new FXMLLoader(EmployeeManageFormController.class.getResource("/view/NotPaidPopupForm.fxml"));
-                    Parent root = loader.load();
-                    innovativePopupPane.getChildren().add(root);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        thread.start();
+        if (isPay) {
+//            try {
+//                FXMLLoader loader = new FXMLLoader(EmployeeManageFormController.class.getResource("/view/PaidPopupForm.fxml"));
+//                Parent root = loader.load();
+//                innovativePopupPane.getChildren().add(root);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
 
+            Navigation.switchPaging(innovativePopupPane, "PaidPopupForm.fxml");
+        } else {
+            try {
+                FXMLLoader loader = new FXMLLoader(EmployeeManageFormController.class.getResource("/view/NotPaidPopupForm.fxml"));
+                Parent root = loader.load();
+                innovativePopupPane.getChildren().add(root);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+
+    public static void renameImgPhone() {
+        String folderPath = "/storage/30A6-324B/DCIM/Camera/";
+
+        String adbListCommand = "adb shell ls " + folderPath;
+
+        boolean isImageFound = false;
+        while (!isImageFound) {
+            try {
+                Process process = Runtime.getRuntime().exec(adbListCommand);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Check if the file is an image (you can use a more specific condition)
+                    if (line.endsWith(".jpg") || line.endsWith(".png") || line.endsWith(".jpeg")) {
+                        // Rename the image file
+                        String oldFilePath = folderPath + line;
+                        String newFilePath = folderPath + "B001.jpg"; // Replace with the new file name
+
+                        String adbRenameCommand = "adb shell mv " + oldFilePath + " " + newFilePath;
+                        Process renameProcess = Runtime.getRuntime().exec(adbRenameCommand);
+                        int exitCode = renameProcess.waitFor();
+
+                        if (exitCode == 0) {
+                            System.out.println("Image renamed successfully!");
+                            isImageFound = true;
+                            break; // Exit the loop after renaming the first image found
+                        } else {
+                            System.err.println("Image renaming failed. Exit code: " + exitCode);
+                        }
+                    }
+                }
+
+                reader.close();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public static void imgTranfer() {
+
+        String adbCommand = "adb pull /storage/30A6-324B/DCIM/Camera/B001.jpg D:\\Solution99";
+        boolean imageFound = false;
+
+        try {
+            while (!imageFound) {
+                Process process = Runtime.getRuntime().exec(adbCommand);
+                int exitCode = process.waitFor();
+                if (exitCode == 0) {
+                    System.out.println("File transfer successful!");
+                    imageFound = true;
+                    System.out.println(adbCommand);
+                } else {
+                    System.err.println("File transfer failed. Exit code: " + exitCode);
+                }
+                Thread.sleep(2000);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void qrDecode(){
+        try {
+            File qrCodeImage = new File("D:\\Solution99\\B001.jpg");
+            BufferedImage bufferedImage = ImageIO.read(qrCodeImage);
+
+            LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+            Result result = new MultiFormatReader().decode(bitmap);
+            String decodedText = result.getText();
+            System.out.println(decodedText);
+
+            TaxManageFormController.getInstance().setInnovativeBoatId(decodedText);
+
+        } catch (IOException | NotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteImgPhone() {
+        String adbCommand = "adb shell rm /storage/30A6-324B/DCIM/Camera/B001.jpg";
+
+        try {
+            Process process = Runtime.getRuntime().exec(adbCommand);
+            int exitCode = process.waitFor();
+
+            if (exitCode == 0) {
+                System.out.println("File deleted successfully!");
+            } else {
+                System.err.println("File deletion failed. Exit code: " + exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteImgLap() {
+        String imagePath = "D:\\Solution99\\B001.jpg"; // Replace with the actual path of your image
+
+        try {
+            Path path = Paths.get(imagePath);
+            Files.deleteIfExists(path);
+            System.out.println("Image deleted successfully.");
+        } catch (NoSuchFileException e) {
+            System.out.println("The image does not exist.");
+        } catch (DirectoryNotEmptyException e) {
+            System.out.println("The specified path is a directory.");
+        } catch (IOException e) {
+            System.out.println("An I/O error occurred.");
+            e.printStackTrace();
+        }
     }
 }
