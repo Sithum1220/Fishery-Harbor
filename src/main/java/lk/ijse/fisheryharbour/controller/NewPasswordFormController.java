2f6c967a80b2e56d106bf83c6637fa303717056c
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-09-21 20:19:35 +0530

    Attendence Update

---
+++ src/main/java/lk/ijse/fisheryharbour/controller/NewPasswordFormController.java
@@ -0,0 +1,20 @@
+package lk.ijse.fisheryharbour.controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class NewPasswordFormController {
+    public JFXTextField txtUserName;
+    public JFXPasswordField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void closeOnMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void minimizeOnMouseClick(MouseEvent mouseEvent) {
+    }
+}
