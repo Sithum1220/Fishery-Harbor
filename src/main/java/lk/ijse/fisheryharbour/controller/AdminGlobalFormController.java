8fda71b8f8efd5776336b3839b85e849a4f5f437
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-09-21 10:47:14 +0530

    AdminGlobalForm BackEnd

--- src/main/java/lk/ijse/fisheryharbour/controller/AdminGlobalFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/AdminGlobalFormController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -47,30 +48,94 @@ public class AdminGlobalFormController implements Initializable {
     }
 
     public void dashboardOnAction(ActionEvent actionEvent) throws IOException {
+        btnSelected(btnDashboard, btnDashboardText, btnDashboardImg, "category.png");
+        btnUnselected(btnAttendance, btnEmployeeText, btnEmployeeImg, "attendence-btn.png");
+        btnUnselected(btnBoatManage, btnSupplierText, btnSupplierImg, "boat-btn.png");
+        btnUnselected(btnRent, btnOwnerText, btnOwnerImg, "user.png");
+        btnUnselected(btnStock, btnStockText, btnStockImg, "box.png");
+
         Navigation.switchPaging(pagingPane,"AdminDashboardForm.fxml");
     }
 
     public void btnAttendenceOnAction(ActionEvent actionEvent) throws IOException {
+        btnUnselected(btnDashboard, btnDashboardText, btnDashboardImg, "category-2.png");
+        btnSelected(btnAttendance, btnEmployeeText, btnEmployeeImg, "calendar-2.png");
+        btnUnselected(btnBoatManage, btnSupplierText, btnSupplierImg, "boat-btn.png");
+        btnUnselected(btnRent, btnOwnerText, btnOwnerImg, "user.png");
+        btnUnselected(btnStock, btnStockText, btnStockImg, "box.png");
+
         Navigation.switchPaging(pagingPane,"AttendanceForm.fxml");
 
     }
 
-    public void btnBoatManageOnAction(ActionEvent actionEvent) {
+    void btnSelected(JFXButton button, Label label, ImageView imageView, String path) {
+        button.setStyle(
+                "-fx-background-color: #EDF3FF;" +
+                        "-fx-background-radius: 12px;");
+        label.setStyle("-fx-text-fill: #1351CB;" +
+                "-fx-font-size: 16px");
+        imageView.setImage(new Image("img/icon/" + path));
     }
 
-    public void btnRentAddOnAction(ActionEvent actionEvent) {
+    void btnUnselected(JFXButton button, Label label, ImageView imageView, String path) {
+        button.setStyle(
+                "-fx-background-radius: 12px;");
+        label.setStyle("-fx-font-weight: 500;" +
+                "-fx-font-size: 16px;" +
+                "-fx-text-fill: #A3A3A3;");
+        imageView.setImage(new Image("img/icon/" + path));
     }
 
-    public void btnStockOnAction(ActionEvent actionEvent) {
+    void logoutBtnSelected(JFXButton button, Label label, ImageView imageView, String path) {
+        button.setStyle(
+                "-fx-background-color: #FFE1E1;" +
+                        "-fx-background-radius: 12px;");
+        label.setStyle("-fx-text-fill: #FF2626;" +
+                "-fx-font-size: 16px");
+        imageView.setImage(new Image("img/icon/" + path));
+    }
+
+    public void btnBoatManageOnAction(ActionEvent actionEvent) throws IOException {
+        btnUnselected(btnDashboard, btnDashboardText, btnDashboardImg, "category-2.png");
+        btnUnselected(btnAttendance, btnEmployeeText, btnEmployeeImg, "attendence-btn.png");
+        btnSelected(btnBoatManage, btnSupplierText, btnSupplierImg, "boat.png");
+        btnUnselected(btnRent, btnOwnerText, btnOwnerImg, "user.png");
+        btnUnselected(btnStock, btnStockText, btnStockImg, "box.png");
+
+        Navigation.switchPaging(pagingPane,"BoatManageForm.fxml");
+    }
+
+    public void btnRentAddOnAction(ActionEvent actionEvent) throws IOException {
+        btnUnselected(btnDashboard, btnDashboardText, btnDashboardImg, "category-2.png");
+        btnUnselected(btnAttendance, btnEmployeeText, btnEmployeeImg, "attendence-btn.png");
+        btnUnselected(btnBoatManage, btnSupplierText, btnSupplierImg, "boat-btn.png");
+        btnSelected(btnRent, btnOwnerText, btnOwnerImg, "user2.png");
+        btnUnselected(btnStock, btnStockText, btnStockImg, "box.png");
+
+        Navigation.switchPaging(pagingPane,"RentDetailsForm.fxml");
+    }
+
+    public void btnStockOnAction(ActionEvent actionEvent) throws IOException {
+        btnUnselected(btnDashboard, btnDashboardText, btnDashboardImg, "category-2.png");
+        btnUnselected(btnAttendance, btnEmployeeText, btnEmployeeImg, "attendence-btn.png");
+        btnUnselected(btnBoatManage, btnSupplierText, btnSupplierImg, "boat-btn.png");
+        btnUnselected(btnRent, btnOwnerText, btnOwnerImg, "user.png");
+        btnSelected(btnStock, btnStockText, btnStockImg, "box-2.png");
+
+        Navigation.switchPaging(pagingPane,"RentDetailsForm.fxml");
     }
 
     public void btnLogoutOnMouseEntered(MouseEvent mouseEvent) {
+        logoutBtnSelected(btnLogout, btnLogoutText, btnLogoutImg, "logout2.png");
     }
 
-    public void btnLogoutOnAction(ActionEvent actionEvent) {
+    public void btnLogoutOnAction(ActionEvent actionEvent) throws IOException {
+        logoutBtnSelected(btnLogout, btnLogoutText, btnLogoutImg, "logout2.png");
+        Navigation.switchNavigation("LoginForm.fxml", actionEvent);
     }
 
     public void btnLogoutOnMouseExit(MouseEvent mouseEvent) {
+        btnUnselected(btnLogout, btnLogoutText, btnLogoutImg, "logout.png");
     }
 
     @Override
