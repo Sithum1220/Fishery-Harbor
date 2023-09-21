f4fa3507ca00e475ba4506676767616f4ba2358c
Sithum Imesh <sithum.imesh20020506@icloud.com>
2023-09-21 10:15:39 +0530

    issue fixed

--- src/main/java/lk/ijse/fisheryharbour/controller/AttendanceAddFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/AttendanceAddFormController.java
@@ -1,11 +1,17 @@
 package lk.ijse.fisheryharbour.controller;
 
-import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import lk.ijse.fisheryharbour.dto.EmployeeAttendanceDTO;
+import lk.ijse.fisheryharbour.model.EmployeeAttendanceModel;
+import lk.ijse.fisheryharbour.utill.DateTimeUtil;
+import lk.ijse.fisheryharbour.utill.Navigation;
+
+import java.sql.SQLException;
 
 public class AttendanceAddFormController {
     public ImageView closeImg;
@@ -13,19 +19,43 @@ public class AttendanceAddFormController {
     public Text txtCancel;
     public JFXTextField EmployeeID;
 
-    public void btnMarkAttendenceOnAction(ActionEvent actionEvent) {
+    public void btnMarkAttendenceOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        EmployeeAttendanceModel employeeAttendanceModel = new EmployeeAttendanceModel();
+        boolean isEmployeeExist = employeeAttendanceModel.isEmployeeExsist(EmployeeID.getText());
+        boolean isEmployeeTodayExist = employeeAttendanceModel.isEmployeeTodayExsist(EmployeeID.getText());
+        if (isEmployeeExist) {
+            if (isEmployeeTodayExist) {
+
+                EmployeeAttendanceDTO employeeAttendanceDTO = new EmployeeAttendanceDTO();
+                employeeAttendanceDTO.setEmployee_Id(EmployeeID.getText());
+                employeeAttendanceDTO.setDate(DateTimeUtil.dateNow());
+                employeeAttendanceDTO.setTime(DateTimeUtil.timeNow());
+                boolean save = employeeAttendanceModel.save(employeeAttendanceDTO);
+
+                Navigation.adminClosePane();
+
+            } else {
+                new Alert(Alert.AlertType.ERROR, "This Employee is Today Exist").showAndWait();
+            }
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Invalid Employee Id").showAndWait();
+        }
     }
 
     public void btnCancelOnAction(ActionEvent actionEvent) {
+        Navigation.adminClosePane();
     }
 
     public void btnCancelOnMouseEnterd(MouseEvent mouseEvent) {
+
     }
 
     public void btnCancelOnMouseExit(MouseEvent mouseEvent) {
     }
 
     public void closeOnMouseClick(MouseEvent mouseEvent) {
+        Navigation.adminClosePane();
+
     }
 
     public void closeOnMouseEnterd(MouseEvent mouseEvent) {
--- src/main/java/lk/ijse/fisheryharbour/controller/AttendanceFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/AttendanceFormController.java
@@ -2,10 +2,15 @@ package lk.ijse.fisheryharbour.controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
+import lk.ijse.fisheryharbour.model.EmployeeAttendanceModel;
 import lk.ijse.fisheryharbour.utill.Navigation;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class AttendanceFormController {
     public JFXButton btnEnterID;
@@ -13,6 +18,33 @@ public class AttendanceFormController {
 
     public void btnEnterIdAddOnAction(ActionEvent actionEvent) throws IOException {
         Navigation.AdminPopupPane("AttendanceAddForm.fxml");
+    }
+
+    public void getAllId() {
+        EmployeeAttendanceModel employeeAttendanceModel = new EmployeeAttendanceModel();
+        ArrayList<String> list = null;
+        try {
+            list = employeeAttendanceModel.getAllId();
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        vBox.getChildren().clear();
+        for (int i = 0; i < list.size(); i++) {
+            loadDataTable(list.get(i));
+        }
+    }
+
+    private void loadDataTable(String id) {
+        try {
+            FXMLLoader loader = new FXMLLoader(EmployeeManageFormController.class.getResource("/view/"));
+            Parent root = loader.load();
+            EmployeeDetailsBarFormController controller = loader.getController();
+            controller.setData(id);
+            vBox.getChildren().add(root);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
