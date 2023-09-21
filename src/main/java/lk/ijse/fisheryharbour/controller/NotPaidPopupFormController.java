d9d4b2c4d840b6e1eb075dc7f7b3a23547907c48
Sithum Imesh <sithum.imesh20020506@icloud.com>
2023-09-22 01:45:00 +0530

    issue fixed

--- src/main/java/lk/ijse/fisheryharbour/controller/NotPaidPopupFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/NotPaidPopupFormController.java
@@ -1,10 +1,20 @@
 package lk.ijse.fisheryharbour.controller;
 
+import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import lk.ijse.fisheryharbour.dto.BoatDTO;
+import lk.ijse.fisheryharbour.dto.OwnerDTO;
+import lk.ijse.fisheryharbour.model.BoatModel;
+import lk.ijse.fisheryharbour.model.OwnerModel;
+import lk.ijse.fisheryharbour.utill.DateTimeUtil;
 import lk.ijse.fisheryharbour.utill.Navigation;
 
-public class NotPaidPopupFormController {
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class NotPaidPopupFormController implements Initializable {
     public Text txtBoatId;
     public Text txtBoatName;
     public Text txtOwnerName;
@@ -13,5 +23,21 @@ public class NotPaidPopupFormController {
     public void closeOnMouseClicked(MouseEvent mouseEvent) {
         TaxManageFormController.getInstance().innovativePopupPane.getChildren().clear();
         TaxManageFormController.getInstance().innovativePopupPane.setVisible(false);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        BoatModel boatModel = new BoatModel();
+        OwnerModel ownerModel = new OwnerModel();
+        try {
+            BoatDTO boatDTO = boatModel.getData(TaxManageFormController.getInstance().innovativeBoatId);
+            OwnerDTO ownerDTO = ownerModel.getData(boatDTO.getOwner_Id());
+            txtBoatId.setText(TaxManageFormController.getInstance().innovativeBoatId);
+            txtBoatName.setText(boatDTO.getBoat_name());
+            txtOwnerName.setText(ownerDTO.getFirst_Name()+" "+ownerDTO.getLast_Name());
+            txtDate.setText(DateTimeUtil.dateNow());
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/lk/ijse/fisheryharbour/controller/PaidPopupFormController.java
+++ src/main/java/lk/ijse/fisheryharbour/controller/PaidPopupFormController.java
@@ -3,9 +3,15 @@ package lk.ijse.fisheryharbour.controller;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import lk.ijse.fisheryharbour.dto.BoatDTO;
+import lk.ijse.fisheryharbour.dto.OwnerDTO;
+import lk.ijse.fisheryharbour.model.BoatModel;
+import lk.ijse.fisheryharbour.model.OwnerModel;
+import lk.ijse.fisheryharbour.utill.DateTimeUtil;
 import lk.ijse.fisheryharbour.utill.Navigation;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class PaidPopupFormController implements Initializable {
@@ -21,5 +27,19 @@ public class PaidPopupFormController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        BoatModel boatModel = new BoatModel();
+        OwnerModel ownerModel = new OwnerModel();
+        try {
+            BoatDTO boatDTO = boatModel.getData(TaxManageFormController.getInstance().innovativeBoatId);
+            OwnerDTO ownerDTO = ownerModel.getData(boatDTO.getOwner_Id());
+            txtBoatId.setText(TaxManageFormController.getInstance().innovativeBoatId);
+            txtBoatName.setText(boatDTO.getBoat_name());
+            txtOwnerName.setText(ownerDTO.getFirst_Name()+" "+ownerDTO.getLast_Name());
+            txtDate.setText(DateTimeUtil.dateNow());
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
